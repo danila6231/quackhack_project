5e5c787fed2ef364d1aa30687d96935db2958870
kaiverson <kaiiverson510@gmail.com>
2025-01-19 08:43:31 -0800

    made vue work on windows

--- prompt_guesser/frontend/urls.py
+++ prompt_guesser/frontend/urls.py
@@ -3,5 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', game_home, name='game_home'),
-    path('/list', game_list, name='game_list'),  # Serve 'index.html' at the root URL
+    path('list', game_list, name='game_list'),  # Serve 'index.html' at the root URL
 ]
