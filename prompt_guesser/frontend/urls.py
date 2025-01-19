dfc1b10abafb30c5e4b75603b0f066a93eb5f694
kaiverson <kaiiverson510@gmail.com>
2025-01-18 19:42:59 -0800

    generic front end to be deleted

--- prompt_guesser/frontend/urls.py
+++ prompt_guesser/frontend/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import game_home
+from .views import *
 
 urlpatterns = [
-    path('', game_home, name='game_home'),  # Serve 'index.html' at the root URL
+    path('', game_home, name='game_home'),
+    path('/list', game_list, name='game_list'),  # Serve 'index.html' at the root URL
 ]
