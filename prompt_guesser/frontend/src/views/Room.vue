4de2b8720eec9436a1028d6d2ccba508ad680025
mdushkoff <mdushkoff@gmail.com>
2025-01-19 07:41:57 -0800

    Room updated with logic for game

--- prompt_guesser/frontend/src/views/Room.vue
+++ prompt_guesser/frontend/src/views/Room.vue
@@ -48,73 +48,165 @@
     <!-- Prompt Chooser -->
     <v-row align="center" v-if="role == 'chooser'">
       <v-col>
-        <!-- Image Button List -->
-        <v-container align="center">
-          <v-row>
-            <v-col>
-              <img class="styled-image" :src="img1">
-            </v-col>
+        <!-- Image Selection -->
+        <div v-if="currentState == 'imageSelection'">
+          <!-- Image Button List -->
+          <v-container align="center">
+            <v-row>
+              <v-col>
+                <button v-on:click="chooseImage1">
+                  <img class="styled-image" :src="img1">
+                </button>
+              </v-col>
 
-            <v-col>
-              <img class="styled-image" :src="img2">
-            </v-col>
+              <v-col>
+                <button v-on:click="chooseImage2">
+                  <img class="styled-image" :src="img2">
+                </button>
+              </v-col>
 
-            <v-col>
-              <img class="styled-image" :src="img3">
-            </v-col>
-          </v-row>
-        </v-container>
+              <v-col>
+                <button v-on:click="chooseImage3">
+                  <img class="styled-image" :src="img3">
+                </button>
+              </v-col>
+            </v-row>
+          </v-container>
 
-        <!-- Prompt Text Field -->
-        <v-container align="center">
-          <v-row>
-            <v-textarea
-              :bg-color="playerNumber == 1 ? 'light-blue' : 'red-lighten-1'"
-              label="Enter a prompt"
-              variant="solo-filled"
-              v-model="promptValue"
-            ></v-textarea>
-          </v-row>
-        </v-container>
+          <!-- Instructions -->
+          <v-container align="center">
+            <v-row>
+              <v-col>
+                <h3>Select an image</h3>
+              </v-col>
+            </v-row>
+          </v-container>
+        </div>
 
-        <!-- Submit Button -->
-        <v-container align="center">
-          <v-row justify="center">
-            <v-btn
-              color="lighten-2"
-              text
-              @click="submitPrompt"
-            >
-              Submit
-            </v-btn>
-          </v-row>
-        </v-container>
+        <!-- Prompt Area -->
+        <transition>
+          <div v-if="currentState == 'prompting'">
+            <!-- Prompt Text Field -->
+            <v-container align="center">
+              <v-row>
+                <v-textarea
+                  :bg-color="playerNumber == 1 ? 'light-blue' : 'red-lighten-1'"
+                  label="Enter a prompt"
+                  variant="solo-filled"
+                  v-model="promptValue"
+                ></v-textarea>
+              </v-row>
+            </v-container>
+
+            <!-- Submit Button -->
+            <v-container align="center">
+              <v-row justify="center">
+                <v-btn
+                  color="lighten-2"
+                  text
+                  @click="submitPrompt"
+                >
+                  Submit
+                </v-btn>
+              </v-row>
+            </v-container>
+          </div>
+        </transition>
       </v-col>
     </v-row>
 
     <!-- Prompt Guesser -->
     <div align="center" v-if="role == 'guesser'">
       <!-- Waiting State -->
-      <v-container fill-height fluid>
-        <v-row align="center" justify="center">
-          <v-progress-circular indeterminate :size="100" :width="10"></v-progress-circular>
-        </v-row>
+      <div v-if="currentState != 'promptGuess' && currentState != 'revealAnswer'">
+        <v-container fill-height fluid>
+          <v-row align="center" justify="center">
+            <v-progress-circular indeterminate :size="100" :width="10"></v-progress-circular>
+          </v-row>
 
-        <v-row align="center" justify="center" class="waiting-text">
-          <h3>Waiting for Player {{ playerNumber.value == 1 ? '2' : '1' }}</h3>
-        </v-row>
-      </v-container>
-
+          <v-row align="center" justify="center" class="waiting-text">
+            <h3>Waiting for Player {{ playerNumber.value == 1 ? '2' : '1' }}</h3>
+          </v-row>
+        </v-container>
+      </div>
 
       <!-- Guessing State -->
+      <transition>
+        <div v-if="currentState == 'promptGuess'">
+          <v-container align="center">
+            <v-row>
+              <v-col>
+                <img class="styled-image" :src="selectedImage">
+              </v-col>
+            </v-row>
+          </v-container>
+
+          <!-- Guess Text Field -->
+          <v-container align="center" class="guess-textbox">
+            <v-row>
+              <v-textarea
+                :bg-color="playerNumber == 1 ? 'light-blue' : 'red-lighten-1'"
+                label="Enter a prompt"
+                variant="solo-filled"
+                v-model="guessValue"
+              ></v-textarea>
+            </v-row>
+          </v-container>
+
+          <!-- Submit Guess Button -->
+          <v-container align="center">
+            <v-row justify="center">
+              <v-btn
+                color="lighten-2"
+                text
+                @click="submitGuess"
+              >
+                Submit
+              </v-btn>
+            </v-row>
+          </v-container>
+        </div>
+      </transition>
       
     </div>
 
     <!-- Reveal Stage -->
     <transition>
-      <v-row>
+      <div v-if="currentState == 'revealAnswer'">
+        <!-- Image -->
+        <v-container align="center">
+          <v-row>
+            <v-col>
+              <img class="styled-image" :src="selectedImage">
+            </v-col>
+          </v-row>
+        </v-container>
 
-      </v-row>
+        <!-- Textboxes -->
+        <v-container align="center">
+          <v-row>
+            <v-col>
+              <v-textarea
+                :bg-color="(role == 'guesser') ? 'light-blue' : 'red-lighten-1'"
+                label=""
+                variant="solo-filled"
+                v-model="styledPrompt"
+                disabled="true"
+              ></v-textarea>
+            </v-col>
+
+            <v-col>
+              <v-textarea
+                :bg-color="(role != 'guesser') ? 'light-blue' : 'red-lighten-1'"
+                label=""
+                variant="solo-filled"
+                v-model="styledPromptGuess"
+                disabled="true"
+              ></v-textarea>
+            </v-col>
+          </v-row>
+        </v-container>
+      </div>
     </transition>
   </v-container>
 </template>
@@ -138,11 +230,16 @@ const round = ref(1); // The current round
 const role = ref(""); // The current role of the player
 const promptValue = ref(""); // Prompt value
 const currentState = ref(""); // The current state
+const guessValue = ref(""); // The current guess
+const promptGuess = ref(""); // Prompt guess output
+const styledPrompt = ref(""); // Prompt final
+const styledPromptGuess = ref(""); // Prompt final guess
 
 // Images
 const img1 = ref("");
 const img2 = ref("");
 const img3 = ref("");
+const selectedImage = ref("");
 
 // Router
 const router = useRouter();
@@ -165,9 +262,55 @@ const submitPrompt = () => {
       img1.value = data.images[0];
       img2.value = data.images[1];
       img3.value = data.images[2];
+      currentState.value = 'imageSelection';
     });
 }
 
+const submitGuess = () => {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ 'prompt': guessValue.value })
+  };
+
+  //   console.log(router.currentRoute._rawValue)
+  var session_id = roomName.value;
+  //console.log(session_id);
+  //console.log(typeof(session_id));
+  fetch('http://127.0.0.1:8000/api/sessions/' + session_id + '/process-guess', requestOptions)
+    .then(response => response.json())
+    .then((data) => {
+      //console.log(data);
+    });
+}
+
+const chooseImage1 = () => {
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ 'image_url': img1.value })
+  };
+
+  //   console.log(router.currentRoute._rawValue)
+  var session_id = roomName.value;
+  //console.log(session_id);
+  //console.log(typeof(session_id));
+  fetch('http://127.0.0.1:8000/api/sessions/' + session_id + '/select-image', requestOptions)
+    .then(response => response)
+    .then((data) => {
+      console.log("CHOSEN");
+      console.log(data);
+      currentState.value = 'promptGuess';
+    });
+}
+
+const chooseImage2 = () => {
+  console.log("Image 2 chosen");
+}
+
+const chooseImage3 = () => {
+  console.log("Image 3 chosen");
+}
 
 // Polling
 var pollState = () => {
@@ -186,7 +329,31 @@ var pollState = () => {
       //console.log(data);
       if (data.player_one_name && data.player_two_name){
         // The game is ready to start
-        isReady.value = true
+        isReady.value = true;
+      }
+
+      // Check for guess indicator
+      if (data.selected_image_url != null && selectedImage.value == ""){
+        selectedImage.value = data.selected_image_url;
+        currentState.value = 'promptGuess';
+      }
+
+      // Check for guess selection
+      if (data.prompt_guess != null && promptGuess.value == ""){
+        promptGuess.value = data.prompt_guess;
+        playerOneScore.value = data.player_one_score;
+        playerTwoScore.value = data.player_two_score;
+        currentState.value = 'revealAnswer';
+      }
+
+      // Check for styled prompt
+      if (data.styled_prompt != null && styledPrompt.value == ""){
+        styledPrompt.value = data.styled_prompt;
+      }
+
+      // Check for styled prompt guess
+      if (data.styled_prompt_guess != null && styledPromptGuess.value == ""){
+        styledPromptGuess.value = data.styled_prompt_guess;
       }
     });
 }
@@ -204,6 +371,7 @@ onMounted(() => {
   else {
     role.value = "guesser";
   }
+  currentState.value = 'prompting';
   console.log("Role: " + role.value);
   //console.log(queryData);
 
