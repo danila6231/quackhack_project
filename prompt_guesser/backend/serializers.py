a1d504bbc900d1a94b138980ea3207d9e0dcd9f4
danila6231 <56220751+danila6231@users.noreply.github.com>
2025-01-19 07:09:28 -0800

    fix

--- prompt_guesser/backend/serializers.py
+++ prompt_guesser/backend/serializers.py
@@ -4,7 +4,7 @@ from .models import Session
 class SessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Session
-        fields = ['session_name', 'player_one_name', 'player_two_name', 'player_one_score', 'player_two_score', 'turn', 'created_at', 'prompt', 'prompt_guess', 'image_url', 'selected_image_url']
+        fields = ['session_name', 'player_one_name', 'player_two_name', 'player_one_score', 'player_two_score', 'turn', 'created_at', 'prompt', 'prompt_guess', 'image_url', 'selected_image_url', 'styled_prompt', 'styled_prompt_guess']
 
 class CreateSessionSerializer(serializers.Serializer):
     player_name = serializers.CharField(max_length=50, required=True)
