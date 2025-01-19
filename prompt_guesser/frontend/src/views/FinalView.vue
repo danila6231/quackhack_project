30f84cc175100f6fa3e18f1388f4a644dc808864
danila6231 <56220751+danila6231@users.noreply.github.com>
2025-01-19 08:10:35 -0800

    final page scratch

---
+++ prompt_guesser/frontend/src/views/FinalView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="final-view">
+    <div id="scores" class="top-bar">
+      <v-row>
+        <!-- P1 Score -->
+        <v-col align="left" style="color: #42a5f5">
+          <div id="p1-score" class="p1-score">
+            <h1>1</h1>
+          </div>
+        </v-col>
+
+        <!-- P2 Score -->
+        <v-col align="right">
+          <div id="p2-score" style="color: #ef5350">
+            <h1>1</h1>
+          </div>
+        </v-col>
+      </v-row>
+      <div align="center">
+        <h1 style="margin-top: 30%">Player 1 winzzz</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+import '../styles/Room.scss';
+import '../styles/FinalView.scss';
+</script>
