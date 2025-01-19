a492b575c648e2ac751b1905dcf0eb020c510fd7
mdushkoff <mdushkoff@gmail.com>
2025-01-19 04:45:33 -0800

    Room testing

--- prompt_guesser/frontend/src/views/Room.vue
+++ prompt_guesser/frontend/src/views/Room.vue
@@ -18,13 +18,14 @@
     </v-container>
   </div>
   
-  
+  <!-- Game Area -->
   <v-container v-if="isReady" id="game-area">
+    <!-- Top Bar -->
     <div id="scores" class="top-bar">
       <v-row>
         <!-- P1 Score -->
         <v-col align="left">
-          <div id="p2-score" class="p1-score">
+          <div id="p1-score" class="p1-score">
             <h1>{{ playerOneScore }}</h1>
           </div>
         </v-col>
@@ -43,6 +44,64 @@
         </v-col>
       </v-row>
     </div>
+
+    <!-- Prompt Chooser -->
+    <v-row align="center" v-if="role == 'chooser'">
+      <v-col>
+        <!-- Image Button List -->
+        
+
+        <!-- Prompt Text Field -->
+        <v-container align="center">
+          <v-row>
+            <v-textarea
+              :bg-color="playerNumber == 1 ? 'light-blue' : 'red-lighten-1'"
+              label="Enter a prompt"
+              variant="solo-filled"
+              v-model="promptValue"
+            ></v-textarea>
+          </v-row>
+        </v-container>
+
+        <!-- Submit Button -->
+        <v-container align="center">
+          <v-row justify="center">
+            <v-btn
+              color="lighten-2"
+              text
+              @click="submitPrompt"
+            >
+              Submit
+            </v-btn>
+          </v-row>
+        </v-container>
+      </v-col>
+    </v-row>
+
+    <!-- Prompt Guesser -->
+    <div align="center" v-if="role == 'guesser'">
+      <!-- Waiting State -->
+      <v-container fill-height fluid>
+        <v-row align="center" justify="center">
+          <v-progress-circular indeterminate :size="100" :width="10"></v-progress-circular>
+        </v-row>
+
+        <v-row align="center" justify="center" class="waiting-text">
+          <h3>Waiting for Player {{ playerNumber.value == 1 ? '2' : '1' }}</h3>
+        </v-row>
+      </v-container>
+
+
+      <!-- Guessing State -->
+      
+    </div>
+
+    <!-- Reveal Stage -->
+    <transition>
+      <v-row>
+
+      </v-row>
+    </transition>
   </v-container>
 </template>
 
@@ -62,10 +121,34 @@ const playerNumber = ref(0); // The current player number
 const playerOneScore = ref(0); // The current player 1 score
 const playerTwoScore = ref(0); // The current player 2 score
 const round = ref(1); // The current round
+const role = ref(""); // The current role of the player
+const promptValue = ref(""); // Prompt value
 
 // Router
 const router = useRouter();
 
+// Methods
+const submitPrompt = () => {
+  console.log(promptValue.value);
+  const requestOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: { 'prompt': promptValue.value }
+  };
+
+  //   console.log(router.currentRoute._rawValue)
+  var session_id = roomName.value;
+  //console.log(session_id);
+  //console.log(typeof(session_id));
+  fetch('http://127.0.0.1:8000/api/sessions/' + session_id + '/process-prompt', requestOptions)
+    .then(response => response)
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+
+// Polling
 var pollState = () => {
   const requestOptions = {
     method: 'GET',
@@ -94,6 +177,13 @@ onMounted(() => {
   roomName.value = fullPath.substring(6, 11);
   var queryData = JSON.parse(decodeURI(arr[1]));
   playerNumber.value = queryData.player;
+  if (playerNumber.value == 1){
+    role.value = "chooser";
+  }
+  else {
+    role.value = "guesser";
+  }
+  console.log("Role: " + role.value);
   //console.log(queryData);
 
   // Poll
